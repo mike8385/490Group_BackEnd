4f60791789a59d626393bc8bc7cb4617fa512beb
Xitlaly-P <jada23dem@gmail.com>
2025-04-17 17:46:02 -0400

    additional config

--- config.py
+++ config.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-MYSQL_HOST = 'localhost' # localhost
-MYSQL_USER = 'root'
-MYSQL_PASSWORD = '123barreca321'
-=======
 MYSQL_HOST = '34.148.167.204' 
 MYSQL_USER = 'root'
 MYSQL_PASSWORD = 'root'
->>>>>>> working_db
 MYSQL_DB = 'clinic_db'

4e03304d7af076b510dc2d269471782409256c3a
Cassandra Lanjwal <cjl23@njit.edu>
2025-04-28 04:15:29 -0400

    figured out swagger lol

--- routes/testing.py
+++ routes/testing.py
@@ -5,6 +5,31 @@ test_bp = Blueprint('test_bp', __name__)
 
 @test_bp.route('/test-db')
 def test_db_connection():
+    """
+    Test database connection
+    ---
+    responses:
+      200:
+        description: Successfully connected to the database
+        schema:
+          type: object
+          properties:
+            status:
+              type: string
+            result:
+              type: array
+              items:
+                type: integer
+      500:
+        description: Failed to connect to the database
+        schema:
+          type: object
+          properties:
+            status:
+              type: string
+            message:
+              type: string
+    """
     try:
         cursor = mysql.connection.cursor()
         cursor.execute("SELECT 1")

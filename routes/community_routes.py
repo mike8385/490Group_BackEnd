461a55020627807f880d19c7e7f7feba32aed10f
Cassandra Lanjwal <cjl23@njit.edu>
2025-05-12 12:39:50 -0400

    updated swagger and updated try/except blocks

--- routes/community_routes.py
+++ routes/community_routes.py
@@ -17,9 +17,22 @@ storage_client = storage.Client()
 def get_posts(post_id):
     """
     Retrieve community post by ID
+    ---
     tags:
       - Community
+    parameters:
+      - name: post_id
+        in: path
+        required: true
+        schema:
+          type: integer
+    responses:
+      200:
+        description: Post data returned
+      404:
+        description: Post not found
     """
+
     cursor = mysql.connection.cursor()
     query = """
         SELECT CP.post_id, CP.meal_id, CP.user_id, CP.description, CP.picture, CP.created_at,
@@ -96,8 +109,20 @@ def get_posts(post_id):
 def get_posts_by_user(user_id):
     """
     Retrieve community posts by user ID
+    ---
     tags:
       - Community
+    parameters:
+      - name: user_id
+        in: path
+        required: true
+        schema:
+          type: integer
+    responses:
+      200:
+        description: List of posts
+      404:
+        description: No posts found
     """
     cursor = mysql.connection.cursor()
     query = """
@@ -169,6 +194,12 @@ def get_posts_by_user(user_id):
 def get_all_posts():
     """
     Retrieve all community posts
+    ---
+    tags:
+      - Community
+    responses:
+      200:
+        description: List of all community posts
     """
     cursor = mysql.connection.cursor()
     query = """
@@ -693,38 +724,41 @@ def get_comments(post_id):
       200:
         description: List of comments
       400:
-        description: Error retrieving comments
+        description: Failed to fetch comments
     """
     cursor = mysql.connection.cursor()
-    query = """
-        SELECT PC.comment_id, PC.post_id, PC.user_id, PC.comment_text, PC.created_at,
-          COALESCE(P.first_name, D.first_name) AS first_name,
-          COALESCE(P.last_name, D.last_name) AS last_name,
-          U.doctor_id, U.patient_id
-        FROM POST_COMMENTS AS PC
-        JOIN USER AS U ON PC.user_id = U.user_id
-        LEFT JOIN PATIENT AS P ON U.patient_id = P.patient_id
-        LEFT JOIN DOCTOR AS D ON U.doctor_id = D.doctor_id
-        WHERE PC.post_id = %s
-        ORDER BY PC.created_at DESC;
-    """
-    cursor.execute(query, (post_id,))
-    comments = cursor.fetchall()
+    try:
+      query = """
+          SELECT PC.comment_id, PC.post_id, PC.user_id, PC.comment_text, PC.created_at,
+            COALESCE(P.first_name, D.first_name) AS first_name,
+            COALESCE(P.last_name, D.last_name) AS last_name,
+            U.doctor_id, U.patient_id
+          FROM POST_COMMENTS AS PC
+          JOIN USER AS U ON PC.user_id = U.user_id
+          LEFT JOIN PATIENT AS P ON U.patient_id = P.patient_id
+          LEFT JOIN DOCTOR AS D ON U.doctor_id = D.doctor_id
+          WHERE PC.post_id = %s
+          ORDER BY PC.created_at DESC;
+      """
+      cursor.execute(query, (post_id,))
+      comments = cursor.fetchall()
 
-    result = []
-    for comment in comments:
-        result.append({
-            "comment_id": comment[0],
-            "post_id": comment[1],
-            "user_id": comment[2],
-            "comment_text": comment[3],
-            "created_at": comment[4],
-            "first_name": comment[5],
-            "last_name": comment[6],
-            "doctor_id": comment[7],
-            "patient_id": comment[8]
-        })
-    return jsonify(result), 200
+      result = []
+      for comment in comments:
+          result.append({
+              "comment_id": comment[0],
+              "post_id": comment[1],
+              "user_id": comment[2],
+              "comment_text": comment[3],
+              "created_at": comment[4],
+              "first_name": comment[5],
+              "last_name": comment[6],
+              "doctor_id": comment[7],
+              "patient_id": comment[8]
+          })
+      return jsonify(result), 200
+    except Exception as e:
+      return jsonify({"Failed to fetch comments. Error": str(e)}), 400
 
 # Save a post/meal
 @comm_bp.route('/posts/save', methods=['POST'])
@@ -765,31 +799,98 @@ def save_post():
     cursor = mysql.connection.cursor()
 
     try:
-        # Insert into saved posts table
-        cursor.execute("""
-            INSERT INTO SAVED_MEAL (user_id, meal_id, post_id)
-            VALUES (%s, %s, %s)
-        """, (user_id, post_id, meal_id))
+      # Insert into saved posts table
+      cursor.execute("""
+          INSERT INTO SAVED_MEAL (user_id, meal_id, post_id)
+          VALUES (%s, %s, %s)
+      """, (user_id, post_id, meal_id))
 
-        mysql.connection.commit()
+      mysql.connection.commit()
 
-        return jsonify({
-            "message": "Post saved successfully.",
-            "post_id": post_id,
-            "user_id": user_id
-        }), 201
+      return jsonify({
+          "message": "Post saved successfully.",
+          "post_id": post_id,
+          "user_id": user_id
+      }), 201
 
     except Exception as e:
-        mysql.connection.rollback()
-        return jsonify({"error": str(e)}), 400
+      mysql.connection.rollback()
+      return jsonify({"Failed to save post. Error": str(e)}), 400
     finally:
-        cursor.close()
+      cursor.close()
 
 # get saved posts/meals by id
 @comm_bp.route('/posts/save/<int:user_id>', methods=['GET'])
 def get_saved(user_id):
     """
     Get saved posts/meals by user_id
+    ---
+    tags:
+      - Community
+    parameters:
+      - name: user_id
+        in: path
+        required: true
+        schema:
+          type: integer
+    responses:
+      200:
+        description: List of saved posts
+      400:
+        description: Failed to fetch saved posts
+    """
+    try:
+      cursor = mysql.connection.cursor()
+      query = """
+          SELECT SM.saved_meal_id, SM.user_id, SM.meal_id, SM.post_id,
+                M.meal_name, M.meal_calories,
+                COALESCE(P.first_name, D.first_name) AS first_name,
+                COALESCE(P.last_name, D.last_name) AS last_name,
+                CONCAT_WS(', ',
+                    (
+                        SELECT GROUP_CONCAT(DISTINCT MP.meal_plan_name SEPARATOR ', ')
+                        FROM MEAL_PLAN_ENTRY AS MPE
+                        JOIN MEAL_PLAN AS MP ON MPE.meal_plan_id = MP.meal_plan_id
+                        WHERE MPE.meal_id = CP.meal_id
+                    ),
+                    CP.add_tag
+                ) AS tag
+          FROM SAVED_MEAL AS SM
+          JOIN MEAL AS M ON SM.meal_id = M.meal_id
+          JOIN USER AS U ON SM.user_id = U.user_id
+          JOIN COMMUNITY_POST AS CP ON SM.post_id = CP.post_id
+          LEFT JOIN PATIENT AS P ON U.patient_id = P.patient_id
+          LEFT JOIN DOCTOR AS D ON U.doctor_id = D.doctor_id
+          WHERE SM.user_id = %s
+          ORDER BY SM.saved_meal_id DESC;
+      """
+      cursor.execute(query, (user_id,))
+      saved_posts = cursor.fetchall()
+
+      result = []
+      for post in saved_posts:
+          result.append({
+              "saved_meal_id": post[0],
+              "user_id": post[1],
+              "meal_id": post[2],
+              "post_id": post[3],
+              "meal_name": post[4],
+              "meal_calories": post[5],
+              "first_name": post[6],
+              "last_name": post[7],
+              "tag": post[8]
+          })
+      return jsonify(result), 200
+    except Exception as e:
+        return jsonify({"Failed to fetch posts due to this error": str(e)}), 400
+    finally:
+        cursor.close()
+
+# if post is saved
+@comm_bp.route('/posts/is-saved', methods=['POST'])
+def is_saved():
+    """
+    Check if a post is saved by a user
     ---
     tags:
       - Community
@@ -800,69 +901,18 @@ def get_saved(user_id):
           schema:
             type: object
             required:
-              - post_id
               - user_id
+              - post_id
             properties:
-              post_id:
-                type: integer
               user_id:
                 type: integer
-            example:
-              post_id: 5
-              user_id: 1
+              post_id:
+                type: integer
     responses:
-      201:
-        description: Post saved successfully
+      200:
+        description: Result indicating if post is saved
       400:
-        description: Input error or database failure
-    """
-    cursor = mysql.connection.cursor()
-    query = """
-        SELECT SM.saved_meal_id, SM.user_id, SM.meal_id, SM.post_id,
-               M.meal_name, M.meal_calories,
-               COALESCE(P.first_name, D.first_name) AS first_name,
-               COALESCE(P.last_name, D.last_name) AS last_name,
-               CONCAT_WS(', ',
-                  (
-                      SELECT GROUP_CONCAT(DISTINCT MP.meal_plan_name SEPARATOR ', ')
-                      FROM MEAL_PLAN_ENTRY AS MPE
-                      JOIN MEAL_PLAN AS MP ON MPE.meal_plan_id = MP.meal_plan_id
-                      WHERE MPE.meal_id = CP.meal_id
-                  ),
-                  CP.add_tag
-              ) AS tag
-        FROM SAVED_MEAL AS SM
-        JOIN MEAL AS M ON SM.meal_id = M.meal_id
-        JOIN USER AS U ON SM.user_id = U.user_id
-        JOIN COMMUNITY_POST AS CP ON SM.post_id = CP.post_id
-        LEFT JOIN PATIENT AS P ON U.patient_id = P.patient_id
-        LEFT JOIN DOCTOR AS D ON U.doctor_id = D.doctor_id
-        WHERE SM.user_id = %s
-        ORDER BY SM.saved_meal_id DESC;
-    """
-    cursor.execute(query, (user_id,))
-    saved_posts = cursor.fetchall()
-
-    result = []
-    for post in saved_posts:
-        result.append({
-            "saved_meal_id": post[0],
-            "user_id": post[1],
-            "meal_id": post[2],
-            "post_id": post[3],
-            "meal_name": post[4],
-            "meal_calories": post[5],
-            "first_name": post[6],
-            "last_name": post[7],
-            "tag": post[8]
-        })
-    return jsonify(result), 200
-
-# if post is saved
-@comm_bp.route('/posts/is-saved', methods=['POST'])
-def is_saved():
-    """
-    Check if a post is saved by a user
+        description: Input error
     """
     data = request.get_json()
     user_id = data.get('user_id')
@@ -883,7 +933,29 @@ def is_saved():
 @comm_bp.route('/posts/is-liked', methods=['POST'])
 def is_liked():
     """
-    Check if a post is saved by a user
+    Check if a post is liked by a user
+    ---
+    tags:
+      - Community
+    requestBody:
+      required: true
+      content:
+        application/json:
+          schema:
+            type: object
+            required:
+              - user_id
+              - post_id
+            properties:
+              user_id:
+                type: integer
+              post_id:
+                type: integer
+    responses:
+      200:
+        description: Result indicating if post is liked
+      400:
+        description: Input error
     """
     data = request.get_json()
     user_id = data.get('user_id')
@@ -903,6 +975,32 @@ def is_liked():
 
 @comm_bp.route('/posts/unsave', methods=['DELETE'])
 def unsave_post():
+    """
+    Unsave a post
+    ---
+    tags:
+      - Community
+    requestBody:
+      required: true
+      content:
+        application/json:
+          schema:
+            type: object
+            required:
+              - user_id
+              - post_id
+            properties:
+              user_id:
+                type: integer
+              post_id:
+                type: integer
+    responses:
+      200:
+        description: Post unsaved successfully
+      400:
+        description: Input error
+    """
+
     data = request.get_json()
     user_id = data.get('user_id')
     post_id = data.get('post_id')
